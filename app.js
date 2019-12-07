bd7d547f340dc775b4b7820001912e37a4f137fb
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-12-08 01:41:37 +0300

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const { signInValidationSettings, signUpValidationSettings } = require('./settings/requestValidation');
